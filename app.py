8b3da70eb7748059c46ab1b1801a1c955f45cc71
Olufemi <femiosinkolu@gmail.com>
2023-02-03 12:41:37 +0100

    Major commit - separate sender and receiver

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cohere 
 import os
+from streamlit_option_menu import option_menu
 cohere_api_key = os.environ['cohere_api_key']
 co = cohere.Client(cohere_api_key)
 from cohere.classify import Example
@@ -24,8 +25,14 @@ examples = [
     Example("You should go for it", "Benign")
     ]
 
-if "chat" not in st.session_state:
-    st.session_state.chat = []
+if "sender_chat" not in st.session_state:
+    st.session_state.sender_chat = []
+
+if "receiver_chat" not in st.session_state:
+    st.session_state.receiver_chat = []
+
+if "prev_message_was_toxic" not in st.session_state:
+    st.session_state.prev_message_was_toxic = False
 
 def Toxicity_detector(text):
     inputs = [text]
@@ -48,18 +55,11 @@ def Detoxify_text(text):
     presence_penalty=0,
     stop_sequences=[],
     return_likelihoods='NONE')
+    print(response.generations[0].text)
     return(response.generations[0].text.split("\n")[1])
 
 
-def main():
-    
-    # ===================== Set page config and background =======================
-    # Main panel setup
-    # Set website details
-    st.set_page_config(page_title ="Project Peace", 
-                       page_icon=':desktop_computer:', 
-                       layout='centered')
-    """## Project Peace"""
+def Receiver():
 
     with st.expander("About"):
         st.write("This App was built for the Cohere 2023 hackathon, Project peace is built on top of Cohere's cutting Edge NLP models, and leveraged to help detect hate speech and detoxify them accross various languages")
@@ -70,15 +70,74 @@ def main():
 
     if submit_button:
         if Toxicity_detector(text_message) == "Benign":
-            st.session_state.chat.append(text_message)
+            st.session_state.receiver_chat.append(text_message)
         else:
-            st.session_state.chat.append(Detoxify_text(text_message))
+            try:
+                st.session_state.receiver_chat.append(Detoxify_text(text_message))
+            except Exception:
+                st.warning("Text retracted, Could not detoxify")
 
-    for i in st.session_state.chat:
+    for i in st.session_state.receiver_chat[-5:]:
+        st.write(i+"\n")
+        print(i)
+
+def Sender():
+
+    with st.expander("About"):
+        st.write("This App was built for the Cohere 2023 hackathon, Project peace is built on top of Cohere's cutting Edge NLP models, and leveraged to help detect hate speech and detoxify them accross various languages")
+
+    with st.form(key = 'form1', clear_on_submit=True):
+        text_message = st.text_area("Write message here")
+        submit_button = st.form_submit_button(label="send")
+
+    if submit_button or st.session_state.prev_message_was_toxic:
+        if Toxicity_detector(text_message) == "Benign":
+            st.session_state.sender_chat.append(text_message)
+        else:
+            st.session_state.prev_message_was_toxic = True
+            st.error("This message is toxic, should i detoxify it?")
+            col1,col2, col3 = st.columns([1,1,1])
+            with col1:
+                if st.button('Yes, detoxify'):
+                    print("Detoxifying.....................................................")
+                    try:
+                        st.session_state.sender_chat.append(Detoxify_text(text_message))
+                        st.session_state.prev_message_was_toxic = False
+                    except Exception:
+                        st.warning("Text retracted, Could not detoxify")
+            with col2:
+                if st.button("No, send as is"):
+                    st.session_state.sender_chat.append(text_message)
+                    st.session_state.prev_message_was_toxic = False
+            with col3:
+                if st.button("Retract message"):
+                    st.success("Message retracted")
+                    st.session_state.prev_message_was_toxic = False
+                    
+    for i in st.session_state.sender_chat[-5:]:
         st.write(i+"\n")
         print(i)
 
 
 
 if __name__ == "__main__":
-    main()
+        # ===================== Set page config and background =======================
+    # Main panel setup
+    # Set website details
+    st.set_page_config(page_title ="Project Peace", 
+                       page_icon=':desktop_computer:', 
+                       layout='centered')
+    """## Project Peace"""
+
+    with st.sidebar:
+        my_page = option_menu(
+            menu_title=None,
+            options = ["Receiver's Mode","Sender's Mode"],
+            icons=["arrow-down-right-circle-fill", "send"],
+        )
+    if my_page == "Receiver's Mode":
+        Receiver()
+    elif my_page == "Sender's Mode":
+        Sender()
+    else:
+        st.write("Noting to see here use the side menu")
